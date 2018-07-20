8f2906db18c702722dc49ea7975d7da8fd508edf
ErykSandecki <eryk787@wp.pl>
2018-07-20 16:43:39 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider} from 'react-redux';
+
+import App from './App'
+import {uploadDataToLocalStorage, downloadStatusFromLocalStorage} from './localStorage.js';
+
+import './index.css';
+
+const reducer = (
+  status = {
+      status: downloadStatusFromLocalStorage(),
+  }, action) => {
+  switch (action.type) {
+      case 'ONLINE':
+          uploadDataToLocalStorage(true, action.email);
+          return {
+              status: true,
+          };
+      case 'OFFLINE':
+          uploadDataToLocalStorage(false);
+          window.location = '/';
+          break;
+      default:
+          return status;
+  }
+};
+  
+
+const store = createStore(
+reducer,
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>, document.getElementById('root'));
+
+
+  
